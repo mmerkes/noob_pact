ffc49102c2d507ca5308522b58fbb147cc1140d1
Empty Arsenal <matt.merkes@gmail.com>
2014-03-25 10:34:33 -0700

    Revised gruntfile and uglified file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function(grunt) {
   'use strict';
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
     simplemocha: {
       options: {
         timeout: 3000,
@@ -29,13 +31,25 @@ module.exports = function(grunt) {
         }
       },
       files: ['Gruntfile.js', 'tests/**/*.js', 'noob_pact.js']
-    }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      my_target: {
+        files: {
+          'noob_pact.min.js': ['noob_pact.js']
+        }
+      }
+    },
   });
 
 
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-
+  grunt.registerTask('build', ['uglify']);
   grunt.registerTask('default', ['simplemocha', 'jshint']);
 };
